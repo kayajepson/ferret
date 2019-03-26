fec8d4c90e20ee115c90b6941fac6afb9d53e39e
Kaya Jepson and Megan Schulte <megan@>
2019-03-26 13:12:02 -0700

    add buttons to change background

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $("button#dark").click(function() {
+
     $("body").addClass("dark-background");
     $("h1").addClass("light-text");
   });
+  $("button#light").click(function() {
+    $("body").remove("dark-background");
+    $("body").addClass("light-background");
+    $("h1").remove("light-text");
+    $("h1").addClass("blue-text");
+  });
+
+
 });
